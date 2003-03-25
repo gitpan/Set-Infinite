7c41206cce1efac5aabf2f1ffa116171f6179959
Flávio Soibelmann Glock <fglock@gmail.com>
2003-03-25 21:16:52 +0000

    Import of FGLOCK/Set-Infinite-0.45 from CPAN.
    gitpan-cpan-distribution: Set-Infinite
    gitpan-cpan-version:      0.45
    gitpan-cpan-path:         FGLOCK/Set-Infinite-0.45.tar.gz
    gitpan-cpan-author:       FGLOCK
    gitpan-cpan-maturity:     released

--- lib/Set/Infinite.pm
+++ lib/Set/Infinite.pm
@@ -24,7 +24,7 @@ use vars qw( @ISA %EXPORT_TAGS @EXPORT_OK @EXPORT $VERSION
 @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } , qw(inf new $inf trace_open trace_close) );
 @EXPORT = qw();
 
-$VERSION = '0.44_04';
+$VERSION = '0.45';
 
 use Set::Infinite::Arithmetic;
 
